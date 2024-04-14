ed9b23bec67e9cb51f22bf5cd8973591697b77ea
Melle <melle@git.com>
2024-04-14 16:13:49 +0200

    Add optional execution role

--- packages/aws-cdk-lib/aws-mwaa/lib/environment.ts
+++ packages/aws-cdk-lib/aws-mwaa/lib/environment.ts
@@ -1,5 +1,6 @@
 import { Construct } from 'constructs';
 import { CfnEnvironment } from './mwaa.generated';
+import * as iam from '../../aws-iam';
 import { IBucket } from '../../aws-s3/lib/bucket';
 import { Resource } from '../../core';
 
@@ -27,6 +28,10 @@ export interface EnvironmentProps {
    * The environment class type.
    */
   readonly environmentClass: EnvironmentClass;
+  /**
+   * The environment's execution role.
+   */
+  readonly executionRole?: iam.IRole;
 }
 
 /**
@@ -53,8 +58,100 @@ export enum EnvironmentClass {
  * A new MWAA environment.
  */
 export class Environment extends Resource {
+
+  public readonly executionRole: iam.IRole;
+  
   constructor(scope: Construct, id: string, props: EnvironmentProps) {
     super(scope, id);
+
+    if (!props.executionRole) {
+      this.executionRole = new iam.Role(this, 'ExecutionRole', {
+        roleName: `AmazonMWAA-${props.name}-ExecutionRole`,
+        assumedBy: new iam.CompositePrincipal(
+          new iam.ServicePrincipal('airflow-env.amazonaws.com'),
+          new iam.ServicePrincipal('airflow.amazonaws.com'),
+        ),
+      });
+
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        actions: ['airflow:PublishMetrics'],
+        resources: [this.stack.formatArn({
+          service: 'airflow',
+          resource: 'environment',
+          resourceName: props.name,
+        })],
+      }));
+  
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        effect: iam.Effect.DENY,
+        actions: ['s3:ListAllMyBuckets'],
+        resources: [props.sourceBucket.bucketArn, props.sourceBucket.arnForObjects('*')],
+      }));
+  
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        actions: ['s3:GetObject*', 's3:GetBucket*', 's3:List*'],
+        resources: [props.sourceBucket.bucketArn, props.sourceBucket.arnForObjects('*')],
+      }));
+  
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        actions: [
+          'logs:CreateLogStream',
+          'logs:CreateLogGroup',
+          'logs:PutLogEvents',
+          'logs:GetLogEvents',
+          'logs:GetLogRecord',
+          'logs:GetLogGroupFields',
+          'logs:GetQueryResults',
+        ],
+        resources: [this.stack.formatArn({
+          service: 'logs',
+          resource: 'log-group',
+          resourceName: `airflow-${props.name}-*`,
+        })],
+      }));
+  
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        actions: ['logs:DescribeLogGroups', 'cloudwatch:PutMetricData'],
+        resources: ['*'],
+      }));
+  
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        actions: [
+          'sqs:ChangeMessageVisibility',
+          'sqs:DeleteMessage',
+          'sqs:GetQueueAttributes',
+          'sqs:GetQueueUrl',
+          'sqs:ReceiveMessage',
+          'sqs:SendMessage',
+        ],
+        resources: [this.stack.formatArn({
+          service: 'sqs',
+          account: '*',
+          resource: 'airflow-celery-*',
+        })],
+      }));
+  
+      this.executionRole.addToPrincipalPolicy(new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'kms:Decrypt',
+          'kms:DescribeKey',
+          'kms:GenerateDataKey*',
+          'kms:Encrypt',
+        ],
+        notResources: [this.stack.formatArn({
+          service: 'kms',
+          resource: 'key',
+          region: '*',
+          resourceName: '*',
+        })],
+        conditions: {
+          StringLike: { 'kms:ViaService': `sqs.${this.stack.region}.amazonaws.com` },
+        },
+      }));
+    } else {
+      this.executionRole = props.executionRole;
+    }
 
     new CfnEnvironment(this, 'Resource', {
       name: props.name,
@@ -62,6 +159,7 @@ export class Environment extends Resource {
       sourceBucketArn: props.sourceBucket.bucketArn,
       dagS3Path: props.dagS3Path,
       environmentClass: props.environmentClass,
+      executionRoleArn: this.executionRole.roleArn,
     });
   }
 }
--- packages/aws-cdk-lib/aws-mwaa/test/environment.test.ts
+++ packages/aws-cdk-lib/aws-mwaa/test/environment.test.ts
@@ -1,5 +1,6 @@
 import { Template } from '../../assertions';
-import * as s3 from '../../aws-s3/lib/bucket';
+import * as iam from '../../aws-iam/lib';
+import * as s3 from '../../aws-s3/lib';
 import * as cdk from '../../core';
 import * as mwaa from '../lib';
 
@@ -10,7 +11,7 @@ describe('Environment', () => {
       const bucket = new s3.Bucket(stack, 'Bucket');
 
       new mwaa.Environment(stack, 'Environment', {
-        name: 'mwaa',
+        name: 'Airflow',
         airflowVersion: mwaa.AirflowVersion.V2_8_1,
         sourceBucket: bucket,
         dagS3Path: 'dags',
@@ -21,11 +22,282 @@ describe('Environment', () => {
 
       template.resourceCountIs('AWS::MWAA::Environment', 1);
       template.hasResourceProperties('AWS::MWAA::Environment', {
-        Name: 'mwaa',
+        Name: 'Airflow',
         AirflowVersion: '2.8.1',
         SourceBucketArn: { 'Fn::GetAtt': ['Bucket83908E77', 'Arn'] },
         DagS3Path: 'dags',
         EnvironmentClass: 'mw1.small',
+        ExecutionRoleArn: { 'Fn::GetAtt': ['EnvironmentExecutionRoleA0E74382', 'Arn'] },
+      });
+
+      template.resourceCountIs('AWS::IAM::Role', 1);
+      template.hasResourceProperties('AWS::IAM::Role', {
+        RoleName: 'AmazonMWAA-Airflow-ExecutionRole',
+        AssumeRolePolicyDocument: {
+          Statement: [
+            {
+              Action: 'sts:AssumeRole',
+              Effect: 'Allow',
+              Principal: {
+                Service: 'airflow-env.amazonaws.com',
+              },
+            }, {
+              Action: 'sts:AssumeRole',
+              Effect: 'Allow',
+              Principal: {
+                Service: 'airflow.amazonaws.com',
+              },
+            },
+          ],
+        },
+      });
+
+      template.resourceCountIs('AWS::IAM::Policy', 1);
+      template.hasResourceProperties('AWS::IAM::Policy', {
+        PolicyName: 'EnvironmentExecutionRoleDefaultPolicy063C3CFA',
+        Roles: [{ Ref: 'EnvironmentExecutionRoleA0E74382' }],
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: 'airflow:PublishMetrics',
+              Effect: 'Allow',
+              Resource: {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:',
+                    {
+                      Ref: 'AWS::Partition',
+                    },
+                    ':airflow:',
+                    {
+                      Ref: 'AWS::Region',
+                    },
+                    ':',
+                    {
+                      Ref: 'AWS::AccountId',
+                    },
+                    ':environment/Airflow',
+                  ],
+                ],
+              },
+            }, {
+              Action: 's3:ListAllMyBuckets',
+              Effect: 'Deny',
+              Resource: [
+                {
+                  'Fn::GetAtt': [
+                    'Bucket83908E77',
+                    'Arn',
+                  ],
+                },
+                {
+                  'Fn::Join': [
+                    '',
+                    [
+                      {
+                        'Fn::GetAtt': [
+                          'Bucket83908E77',
+                          'Arn',
+                        ],
+                      },
+                      '/*',
+                    ],
+                  ],
+                },
+              ],
+            },
+            {
+              Action: [
+                's3:GetObject*',
+                's3:GetBucket*',
+                's3:List*',
+              ],
+              Effect: 'Allow',
+              Resource: [
+                {
+                  'Fn::GetAtt': [
+                    'Bucket83908E77',
+                    'Arn',
+                  ],
+                },
+                {
+                  'Fn::Join': [
+                    '',
+                    [
+                      {
+                        'Fn::GetAtt': [
+                          'Bucket83908E77',
+                          'Arn',
+                        ],
+                      },
+                      '/*',
+                    ],
+                  ],
+                },
+              ],
+            },
+            {
+              Action: [
+                'logs:CreateLogStream',
+                'logs:CreateLogGroup',
+                'logs:PutLogEvents',
+                'logs:GetLogEvents',
+                'logs:GetLogRecord',
+                'logs:GetLogGroupFields',
+                'logs:GetQueryResults',
+              ],
+              Effect: 'Allow',
+              Resource: {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:',
+                    {
+                      Ref: 'AWS::Partition',
+                    },
+                    ':logs:',
+                    {
+                      Ref: 'AWS::Region',
+                    },
+                    ':',
+                    {
+                      Ref: 'AWS::AccountId',
+                    },
+                    ':log-group/airflow-Airflow-*',
+                  ],
+                ],
+              },
+            },
+            {
+              Action: [
+                'logs:DescribeLogGroups',
+                'cloudwatch:PutMetricData',
+              ],
+              Effect: 'Allow',
+              Resource: '*',
+            },
+            {
+              Action: [
+                'sqs:ChangeMessageVisibility',
+                'sqs:DeleteMessage',
+                'sqs:GetQueueAttributes',
+                'sqs:GetQueueUrl',
+                'sqs:ReceiveMessage',
+                'sqs:SendMessage',
+              ],
+              Effect: 'Allow',
+              Resource: {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:',
+                    {
+                      Ref: 'AWS::Partition',
+                    },
+                    ':sqs:',
+                    {
+                      Ref: 'AWS::Region',
+                    },
+                    ':*:airflow-celery-*',
+                  ],
+                ],
+              },
+            },
+            {
+              Action: [
+                'kms:Decrypt',
+                'kms:DescribeKey',
+                'kms:GenerateDataKey*',
+                'kms:Encrypt',
+              ],
+              Condition: {
+                StringLike: {
+                  'kms:ViaService': {
+                    'Fn::Join': [
+                      '',
+                      [
+                        'sqs.',
+                        {
+                          Ref: 'AWS::Region',
+                        },
+                        '.amazonaws.com',
+                      ],
+                    ],
+                  },
+                },
+              },
+              Effect: 'Allow',
+              NotResource: {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:',
+                    {
+                      Ref: 'AWS::Partition',
+                    },
+                    ':kms:*:',
+                    {
+                      Ref: 'AWS::AccountId',
+                    },
+                    ':key/*',
+                  ],
+                ],
+              },
+            },
+          ],
+        },
+      });
+    });
+
+    test('execution role attached', () => {
+      const stack = new cdk.Stack();
+      const bucket = new s3.Bucket(stack, 'Bucket');
+
+      const executionRole = new iam.Role(stack, 'ExecutionRole', {
+        roleName: 'AttachedExecutionRole',
+        assumedBy: new iam.CompositePrincipal(
+          new iam.ServicePrincipal('airflow-env.amazonaws.com'),
+          new iam.ServicePrincipal('airflow.amazonaws.com'),
+        ),
+      });
+
+      new mwaa.Environment(stack, 'Environment', {
+        name: 'Airflow',
+        airflowVersion: mwaa.AirflowVersion.V2_8_1,
+        sourceBucket: bucket,
+        dagS3Path: 'dags',
+        environmentClass: mwaa.EnvironmentClass.MW1_SMALL,
+        executionRole,
+      });
+
+      const template = Template.fromStack(stack);
+
+      template.resourceCountIs('AWS::MWAA::Environment', 1);
+      template.hasResourceProperties('AWS::MWAA::Environment', {
+        ExecutionRoleArn: { 'Fn::GetAtt': ['ExecutionRole605A040B', 'Arn'] },
+      });
+
+      template.resourceCountIs('AWS::IAM::Role', 1);
+      template.hasResourceProperties('AWS::IAM::Role', {
+        RoleName: 'AttachedExecutionRole',
+        AssumeRolePolicyDocument: {
+          Statement: [
+            {
+              Action: 'sts:AssumeRole',
+              Effect: 'Allow',
+              Principal: {
+                Service: 'airflow-env.amazonaws.com',
+              },
+            }, {
+              Action: 'sts:AssumeRole',
+              Effect: 'Allow',
+              Principal: {
+                Service: 'airflow.amazonaws.com',
+              },
+            },
+          ],
+        },
       });
     });
   });
